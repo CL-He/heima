364d8d41fe16e59b96215352ded20f64504d5734
Hechunli <Hechunli@qq.com>
2019-12-18 12:23:45 +0800

    03-按钮设置,组件引入

---
+++ src/components/hm-button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="btn" @click="handlerclick">{{text}}</div>
+</template>
+
+<script>
+export default {
+  // text文本的内容
+  props: ['text'],
+  methods: {
+    // 当被单击的时候,将事件传递给父组件,让父组件(login.vue文件)进行事件响应
+    handlerclick (event) {
+      this.$emit('click', event)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.btn{
+    //vw:viewport width:视口宽度,不管是什么屏幕,视口宽度都会分为100份
+    //vh:viewport height:视口宽度
+    // 在设置元素宽高的时候,可以参照UI稿件中的元素的实际大小进行大小设置
+    width: 314*100vw/360;
+    height: 48*100vw/360;
+    background-color: rgb(204,51,0);
+    border-radius: 24*100vw/360;
+    color:#fff;
+    text-align: center;
+    line-height:  48*100vw/360;
+    font-size: 18px;
+}
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,76 @@
 <template>
-  <div class="login">登录</div>
+  <div id="app">
+    <div class="container">
+      <div class="close">
+        <span class="iconfont iconicon-test"></span>
+      </div>
+      <div class="logo">
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="inputs">
+        <input placeholder="请输入手机号" class="input" />
+        <input placeholder="密码" class="input" type="password" />
+      </div>
+      <p class="tips">
+        没有账号？
+        <a href="#/register" class>去注册</a>
+      </p>
+      <!-- 使用 -->
+      <hmbutton text='登录' @click="login"></hmbutton>
+    </div>
+  </div>
 </template>
 
 <script>
+// 引入按钮文件
+import hmbutton from '@/components/hm-button.vue'
 export default {
-
+  // 注册
+  components: {
+    hmbutton
+  },
+  methods: {
+    login (event) {
+      console.log(event)
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='less'>
+.container {
+  padding: 20px;
+}
 
+.close {
+  span {
+    font-size: 27 / 360 * 100vw;
+  }
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+
+  span {
+    display: block;
+    font-size: 126 / 360 * 100vw;
+    color: #d81e06;
+  }
+}
+
+.inputs {
+  input {
+    margin-bottom: 20px;
+  }
+}
+
+.tips {
+  text-align: right;
+  margin-bottom: 20px;
+
+  a {
+    color: #3385ff;
+  }
+}
 </style>
